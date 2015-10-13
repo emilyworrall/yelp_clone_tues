7c0b2655c56a3a667a31d3edb246835456c02570
Emily Worrall <worrall.emily@gmail.com>
2015-10-13 12:08:31 +0100

    reviews are deleted upon destroying restaurant

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
+
+  it 'removes reviews if restaurant is deleted' do
+    restaurant = Restaurant.create(name: 'KFC')
+    review = Review.create(thoughts: 'so so')
+    restaurant.reviews << review
+    expect { restaurant.destroy }.to change {Review.count}
+  end
 end
