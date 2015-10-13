f277119f0d168a9c780bb90c4585c5026bc56174
Usman Jamil <usman.jamil@live.co.uk>
2015-10-13 12:28:01 +0100

    added validation for reviews

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -5,7 +5,7 @@ describe Restaurant, type: :model do
 
   it 'removes reviews if restaurant is deleted' do
     restaurant = Restaurant.create(name: 'KFC')
-    review = Review.create(thoughts: 'so so')
+    review = Review.create(thoughts: 'so so', rating: '2')
     restaurant.reviews << review
     expect { restaurant.destroy }.to change {Review.count}
   end
@@ -14,5 +14,11 @@ describe Restaurant, type: :model do
     restaurant = Restaurant.new(name: "kf")
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
+  end
+
+  it "is not valid unless it has a unique name" do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
   end
 end
