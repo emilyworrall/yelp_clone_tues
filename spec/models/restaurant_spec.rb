89ef4f74a6962d777a1047023b0f647e661ecba5
Usman Jamil <usman.jamil@live.co.uk>
2015-10-13 12:16:44 +0100

    added validation to restaurant

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -9,4 +9,10 @@ describe Restaurant, type: :model do
     restaurant.reviews << review
     expect { restaurant.destroy }.to change {Review.count}
   end
+
+  it 'is not valid with a name of less than three characters' do
+    restaurant = Restaurant.new(name: "kf")
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
+  end
 end
